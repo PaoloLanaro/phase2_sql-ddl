cfb3e9a67572c325a8393944b135eed348948a67
Paolo Lanaro <paololanaro04@gmail.com>
2024-05-26 11:18:10 +0200

    added 'multi-value' emails attribute for users in the form of a table.

--- AppSchema.sql
+++ AppSchema.sql
@@ -8,11 +8,16 @@ CREATE TABLE IF NOT EXISTS users
 (
   user_id INT PRIMARY KEY,
   created_at DATETIME,
-  email VARCHAR(255) UNIQUE NOT NULL
+  home_country VARCHAR(255),
+  gender VARCHAR(255)
 );
 
-DROP TABLE IF EXISTS asdf;
-CREATE TABLE IF NOT EXISTS asdf
+DROP TABLE IF EXISTS user_emails;
+CREATE TABLE IF NOT EXISTS user_emails
 (
-
+  user_id INT,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
 );
